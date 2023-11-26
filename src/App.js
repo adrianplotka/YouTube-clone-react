b28143a0a29ac007edc611e03a83a5374c899aa9
adrianplotka <115931816+adrianplotka@users.noreply.github.com>
2023-11-26 22:55:54 +0100

    create a routing in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Nav from "./common/Nav";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./common/Home";
+import VideoDetail from "./common/VideoDetail";
+import SearchFeed from "./common/SearchFeed";
+import ChannelDetail from "./common/ChannelDetail";
 
 
 const App = () => (
-  <Nav/>
+  <HashRouter>
+    <Nav />
+    <Routes>
+      <Route path="/home" exact element={<Home />} />
+      <Route path="/video/:id" element={<VideoDetail />} />
+      <Route path="/channel/:id" element={<ChannelDetail />} />
+      <Route path="/search/:searchTerm" element={<SearchFeed />} />
+    </Routes>
+  </HashRouter>
+
 )
 export default App;
--- src/common/Nav/index.js
+++ src/common/Nav/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { StyledDiv, Wrapper, Logo } from "./styled";
-import { HashRouter, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Search } from "../Search";
 
 
 const Nav = () => (
-  <HashRouter basename="/">
-    <StyledDiv>
-      <Wrapper>
-        <NavLink exact to="/">
-          <Logo />
-        </NavLink>
-      </Wrapper>
-      <Search />
-    </StyledDiv>
-  </HashRouter>
-
+  <StyledDiv>
+    <Wrapper>
+      <NavLink exact to="/">
+        <Logo />
+      </NavLink>
+    </Wrapper>
+    <Search />
+  </StyledDiv>
 )
 export default Nav;
